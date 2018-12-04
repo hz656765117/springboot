ca4aa655417f8750b2af11f0be1fe193d839befc
huangzhuo <656765117@qq.com>
2018-12-04 15:53:07 +0800

    关闭自动签到功能

--- Microservices/src/main/java/com/hz/springboot/base/quartz/ScheduleTask.java
+++ Microservices/src/main/java/com/hz/springboot/base/quartz/ScheduleTask.java
@@ -16,20 +16,20 @@ public class ScheduleTask {
         LOGGER.info("Hello world, i'm the king of the world!!!");
 
 
-        String  res = HttpUtil.get("https://wobaifu.10010.com/wc/memSign","WOWEALTH_JSESSIONID=2cd86251-5dbd-4858-a4b5-0b76aca12743");
-        LOGGER.info("huangzhuo's wbf sign result{}",res);
-
-        res = HttpUtil.get("https://wobaifu.10010.com/wc/memSign","WOWEALTH_JSESSIONID=5fa4cce0-1bb7-48dd-a0c8-12fb351a2ba1");
-        LOGGER.info("huangzhuo's wqb sign result{}",res);
-
-        res = HttpUtil.get("https://wobaifu.10010.com/wc/memSign","WOWEALTH_JSESSIONID=ed0b8b61-9d34-46db-8098-2092a32255e5");
-        LOGGER.info("zhiyuan's wbf sign result{}",res);
-
-        res = HttpUtil.get("https://wobaifu.10010.com/wc/memSign","WOWEALTH_JSESSIONID=14ccb88b-2799-44e6-b2b0-9c4ea5b87669");
-        LOGGER.info("zhiyuan's wqb sign result{}",res);
-
-        LOGGER.info("test webhooks");
-        LOGGER.info("test webhooks again");
+//        String  res = HttpUtil.get("https://wobaifu.10010.com/wc/memSign","WOWEALTH_JSESSIONID=2cd86251-5dbd-4858-a4b5-0b76aca12743");
+//        LOGGER.info("huangzhuo's wbf sign result{}",res);
+//
+//        res = HttpUtil.get("https://wobaifu.10010.com/wc/memSign","WOWEALTH_JSESSIONID=5fa4cce0-1bb7-48dd-a0c8-12fb351a2ba1");
+//        LOGGER.info("huangzhuo's wqb sign result{}",res);
+//
+//        res = HttpUtil.get("https://wobaifu.10010.com/wc/memSign","WOWEALTH_JSESSIONID=ed0b8b61-9d34-46db-8098-2092a32255e5");
+//        LOGGER.info("zhiyuan's wbf sign result{}",res);
+//
+//        res = HttpUtil.get("https://wobaifu.10010.com/wc/memSign","WOWEALTH_JSESSIONID=14ccb88b-2799-44e6-b2b0-9c4ea5b87669");
+//        LOGGER.info("zhiyuan's wqb sign result{}",res);
+//
+//        LOGGER.info("test webhooks");
+//        LOGGER.info("test webhooks again");
 
     }
 
